2fbb6c50ffdf1007b8b1e7696f885c2e62636b64
jacobhmurphy <mrjacobmurphy@gmail.com>
2019-11-19 09:09:24 -0500

    setting up server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen("3000", err => {
+  if (err) throw err;
+  console.log("Listening on port 3000.");
+});
