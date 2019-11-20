80502e2f3e6c4d5c2fce0633532d3367a4c274a9
Jacob Murphy <mrjacobmurphy@gmail.com>
2019-11-20 14:51:24 -0500

    creating links for algorithm

---
+++ public/alg.js
@@ -0,0 +1,10 @@
+var x = document.querySelector('#formControlRange1').value
+console.log(x)
+
+var y = document.querySelector('#formControlRange2').value
+console.log(y)
+
+// grab values from input
+
+// algorithm
+// if values are < 5 then X. if values are > 5 && < 10 then Y. if values > 10 then Z. 
