31cde41891d048fa8ec9aab9a3e022eb7449981c
Jacob Murphy <mrjacobmurphy@gmail.com>
2019-11-20 13:55:59 -0500

    creating signup page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ routes.get("/", (req, res) => {
     res.render("home.ejs");
   });
 
+routes.get("/signup", (req, res) => {
+  res.render("signup.ejs");
+})
+
+routes.post("/signup", (req, res) => {
+  console.log("hitting the post route on signup page")
+})
+
 module.exports = routes;
