1b5cab6daa670557e93aba41f2ec45d55a54706b
Jacob Murphy <mrjacobmurphy@gmail.com>
2019-11-21 10:32:30 -0500

    adding todos

--- app.js
+++ app.js
@@ -3,22 +3,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const routes = require('./routes/index.js');
 const db = require('./models/index.js');
-const passport = require('./config/passport');
 const session = require('express-session');
-const flash = require('connect-flash');
+const passport = require('./config/passport');
 
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
-app.use(flash());
+app.use(
+  session({ secret: 'cats', resave: true, saveUninitialized: true }),
+);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(
-  session({
-    secret: 'keyboard cat',
-    resave: true,
-    saveUninitialized: true,
-  }),
-);
 app.use(passport.initialize());
 app.use(passport.session());
 
