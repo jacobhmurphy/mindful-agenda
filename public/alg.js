e28fde0269060652fa3fd18115dae5a4bd4723aa
Jacob Murphy <mrjacobmurphy@gmail.com>
2019-11-21 11:34:59 -0500

    trying to write alg

--- public/alg.js
+++ public/alg.js
@@ -4,7 +4,12 @@ console.log(x);
 var y = document.querySelector('#formControlRange2').value;
 console.log(y);
 
+var z = document.querySelector('#guidedOrNot').value;
+console.log(z);
+
 // grab values from input
 
 // algorithm
 // if values are < 5 then X. if values are > 5 && < 10 then Y. if values > 10 then Z.
+
+// should I run this on a server?
